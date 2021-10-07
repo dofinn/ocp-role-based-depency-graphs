0b4650d336bd7590fd5ba4848fc2879559783d02
Dominic Finn <dofinn@redhat.com>
2021-10-07 22:38:07 +1000

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# control-plane-depency-graph
---
+++ cp-dep.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import graphviz
+
+g = graphviz.Digraph('G', filename='control-plane.gv', engine='sfdp')
+
+g.edge('kube-api', 'etcd')
+g.edge('openshift-api', 'etcd')
+g.edge('kube-apiserver-operator', 'kube-api')
+g.edge('etcd-operator', 'kube-api')
+g.edge('kube-controller-manager', 'kube-api')
+g.edge('kube-controller-manager-operator', 'kube-api')
+g.edge('openshift-kube-scheduler', 'kube-api')
+g.edge('openshift-kube-scheduler-operator', 'kube-api')
+g.edge('openshift-apiserver-operator', 'kube-api')
+g.edge('openshift-controller-manager', 'kube-api')
+g.edge('openshift-controller-manager-operator', 'kube-api')
+g.edge('openshift-oauth-apiserver', 'openshift-api')
+g.edge('openshift-authentication', 'openshift-api')
+g.edge('openshift-authentication-operator', 'openshift-api')
+
+
+g.view()
